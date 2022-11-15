e40d99e8163aeffc5c1a542d97691dab8db5b11a
btpxii <btpaul12@gmail.com>
2022-11-14 21:46:25 -0700

    test new branch

--- list_movies/views.py
+++ list_movies/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request):
-    return HttpResponse('Home page goes here. Include links to scraper and manual input forms. Display all movies from out database, in user-defined order. Allow users to change movie ranking by dragging and dropping.')
+    return HttpResponse('Home page goes here. Include links to scraper and manual input forms. Display all movies from out database, in user-defined order. Allow users to change movie ranking by dragging and dropping. ')
